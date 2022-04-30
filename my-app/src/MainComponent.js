01a628052c17e4afcd9ba84af59ed61c1d8fa3ee
Kale Leach <kleach1212@gmail.com>
2022-04-30 15:25:10 -0700

    Commit 5: Partial completion of CurtainMenu

--- my-app/src/MainComponent.js
+++ my-app/src/MainComponent.js
@@ -1,13 +1,14 @@
 import React from "react";
-import './MainComponent.css';
+import "./MainComponent.css"
 
 function MainComponent(){
 
   return(
     <div class = "maincomponent">
-      <h1>Find your School...</h1>
-      <select name="Schools" id="Schools">
-        <option value="UCSD">University of Califonria San Diego</option>
+      <h2>Find Your School...</h2>
+      <select name="Schools" id="Schools" placeholder="Choose School">
+        <option value="">Please Choose...</option>
+        <option value="UCSD">University of California San Diego</option>
         <option value="SDSU"> San Diego State University</option>
         <option value="USD">University of San Diego</option>
       </select>
