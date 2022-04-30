79be8362d23e0a8f68d078d9272009b06b9cfc1c
Kale Leach <kleach1212@gmail.com>
2022-04-30 13:11:28 -0700

    Commit 3: New Components

---
+++ my-app/src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(){
+
+  return(
+    <h1>Hi</h1>
+  )
+}
+
+export default Header
---
+++ my-app/src/MainComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MainComponent(){
+
+  return(
+    <h1>Hello</h1>
+  )
+}
+
+export default MainComponent
