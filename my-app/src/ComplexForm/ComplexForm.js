1ff4eb054f5e0263e57a8322807a3673a759f2b4
Kale Leach <kleach1212@gmail.com>
2022-05-05 17:19:11 -0700

    Commit 14: Added styling to complex form

--- my-app/src/ComplexForm/ComplexForm.js
+++ my-app/src/ComplexForm/ComplexForm.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import "./ComplexForm.css"
 
 function ComplexForm(){
 
   return(
-    <div> hi</div>
+    <form>
+      <div class ="formdiv">
+      <input  class = "formbox "type="text" placeholder="Name"/>
+      <input  class = "formbox "type="text" placeholder="Name"/>
+      <input  class = "formbox "type="text" placeholder="Name"/>
+      <input  class = "formbox "type="text" placeholder="Name"/>
+      <input  class = "formbox "type="text" placeholder="Name"/>
+      <button type="submit">Add Complex</button>
+      </div>
+    </form>
   )
 }
 
